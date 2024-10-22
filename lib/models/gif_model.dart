dd0fab3fac0b76f6824c9d98b67982523310e31e
Lauris <Lauris2232@gmail.com>
2024-10-22 09:01:30 +0300

    e

--- lib/models/gif_model.dart
+++ lib/models/gif_model.dart
@@ -1,13 +1,9 @@
-// models/gif_model.dart
-
-// This class represents a GIF model
 class GifModel {
   final String id;
   final String title;
   final String url;
-  final String user; // Username of the uploader
+  final String user;
 
-  // Constructor for GifModel, requires id, title, url, and user
   GifModel({
     required this.id,
     required this.title,
@@ -15,13 +11,11 @@ class GifModel {
     required this.user,
   });
 
-  // Factory method to create a GifModel from JSON data
   factory GifModel.fromJson(Map<String, dynamic> json) {
     return GifModel(
-      id: json['id'], // Get the GIF ID from JSON
-      title: json['title'], // Get the title from JSON
-      url: json['images']['fixed_height']['url'], // Get the URL for the GIF image
-      user: json['username'] ?? 'Unknown', // Get the uploader's username, fallback to 'Unknown' if empty
-    );
+      id: json['id'], 
+      title: json['title'],
+      url: json['images']['fixed_height']['url'], 
+      user: json['username'] ?? 'Unknown', );
   }
 }
