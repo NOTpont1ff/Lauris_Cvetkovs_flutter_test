15f760e642fc3b3551eab7d413629e4eabf28839
Lauris <Lauris2232@gmail.com>
2024-10-22 10:18:46 +0300

    Revert "a"
    This reverts commit 2cac263c8aea4979f6202cb888e558a434438c0b.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SearchScreen(), 
+      home: SearchScreen(), // Set SearchScreen as the home screen
     );
   }
 }
